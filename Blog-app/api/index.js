aa098dcae97196675a69b8e35731e456bc9dbdf5
Umesh <umeshmane575@gmail.com>
2024-01-17 16:41:16 +0530

    mongodb is ocnnected

--- Blog-app/api/index.js
+++ Blog-app/api/index.js
@@ -1,11 +1,23 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+mongoose.connect(process.env.MONGO)
+  .then(() => {
+    console.log('Connected to MongoDB successfully');
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
+  });
+
 
 const app = express();
 
 app.listen(3000 , () => {
-    console.log('server is runnig on port 3000  kk ');
+    console.log('server is runnig on port 3000  ');
 });
 
 
 
-
+// DATABASE='mongodb://127.0.0.1:27017/service'
