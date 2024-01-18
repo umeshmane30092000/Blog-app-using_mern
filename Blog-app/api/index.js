5ba3ef4997b13506d4717e8538095b8200ed2681
Umesh <umeshmane575@gmail.com>
2024-01-18 15:15:08 +0530

    authetication is created

--- Blog-app/api/index.js
+++ Blog-app/api/index.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 dotenv.config();
 import userRoutes from './routes/user.route.js';
+import authRoutes from './routes/auth.route.js';
 
 mongoose.connect(process.env.MONGO)
   .then(() => {
@@ -14,12 +15,16 @@ mongoose.connect(process.env.MONGO)
 
 
 const app = express();
+// this bleow line allow us to send joson data
+app.use(express.json());
 
-app.listen(3000 , () => {
-    console.log('server is runnig on port 3000  ');
+app.listen(3000, () => {
+  console.log('server is runnig on port 3000  ');
 });
 
 
 app.use('/api/user', userRoutes);
+app.use('/api/auth', authRoutes);
+
 
 // DATABASE='mongodb://127.0.0.1:27017/service'
