04cf3a40798c8b2355f83e693b4408c29c151024
Umesh <umeshmane575@gmail.com>
2024-01-19 17:57:27 +0530

    create a signup api route by hashing password

--- Blog-app/api/controllers/auth.controller.js
+++ Blog-app/api/controllers/auth.controller.js
@@ -1,4 +1,5 @@
 import User from "../models/user.model.js";
+import bcryptjs from 'bcryptjs';
 
 export const signup = async(req, res) => {
     
@@ -8,10 +9,12 @@ export const signup = async(req, res) => {
 
   }
 
+  const hashedPassword = bcryptjs.hashSync(password , 10); 
+
   const newUser = new User({
     username, 
     email,
-    password,
+    password: hashedPassword ,
   });
 
   try {
